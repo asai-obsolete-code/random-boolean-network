764f0b45993521d6e046a03298b2c28df8a4e376
guicho2.71828 <guicho2.71828@gmail.com>
2013-02-21 22:41:41 +0900

    trying to implement with integer and bit manipulation

--- src/random-boolean-network.lisp
+++ src/random-boolean-network.lisp
@@ -5,23 +5,49 @@
 
 (in-package :cl-user)
 (defpackage random-boolean-network
-  (:use :cl :annot :annot.doc :iterate))
+  (:use :cl :annot :annot.doc :iterate :guicho-utilities))
 (in-package :random-boolean-network)
 
 ;; blah blah blah.
 
-;; @doc "the number of inputs on each node"
-;; (defparameter *k* 2)
+@doc "the number of inputs on each node"
+(defparameter *k* 3)
 
-;; @doc "the number of nodes"
-;; (defparameter *n* 4)
+@doc "the number of nodes"
+(defparameter *n* 4)
 
-;; (defun make-bit-array (k)
-;;   (make-array k :element-type 'bit :initial-element 0))
+(defun make-bit-array (n)
+  (make-array n :element-type 'bit :initial-element 0))
 
-;; (defun make-random-bit-array (k)
-;;   (let ((a (make-bit-array k)))
-;; 	(iter (for i below k)
-;; 		  (setf (aref a i) (random 2) ))
-;; 	a))
+(defun make-random-bit-array (n)
+  (let ((a (make-bit-array n)))
+	(iter (for i below n)
+		  (setf (aref a i) (random 2) ))
+	a))
 
+(defun make-random-mask (n k)
+  (let ((a 0))
+	(iter (generating j to (- k 2))
+		  (for i = (random n))
+		  (unless (logbitp i a)
+			(setf (ldb (byte 1 i) a) 1)
+			(next j)))
+	a))
+
+(defclass rbn-node ()
+  ((input-mask :initform (make-random-mask *n* *k*))
+   (array :initform (random *n*))))
+
+(defun propagate (node states)
+  (with-slots (array input-mask) node
+	
+  
+
+
+(defmethod propagate ((node rbn-node))
+  (with-slots (state nodes array input-indexes) node
+	(setf state (aref array (%parse-indexes nodes input-indexes))))
+
+(%parse-indexes (vector (new 'rbn-node :input-indexes '(1) :state 1)
+						(new 'rbn-node :input-indexes '(0) :state 1))
+					'(0 1))
